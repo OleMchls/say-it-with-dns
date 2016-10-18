82f58df7c6c34cfab2687644a8d2cb9bdee280e4
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-10-19 00:21:16 +0200

    add some pieces of the puzzle
    now PUZZLE

--- lib/dnsay.ex
+++ lib/dnsay.ex
@@ -1,2 +1,26 @@
 defmodule Dnsay do
+
+  def transform_to_dns_valid_string(string) do
+    string
+    |> String.normalize(:nfd)
+    |> String.replace(~r/[^A-z\s]/u, "")
+    |> String.replace(~r/\s/, Application.get_env(:dnsay, :whitespace))
+  end
+
+  def chunk(string, chunk_size) do
+    string
+    |> String.split
+    |> Enum.chunk(chunk_size, chunk_size, [])
+    |> Enum.map(&(Enum.join(&1, " ")))
+  end
+
+  def build_tuple_list(list, start) do
+    do_build_tuple_list(start, list, [])
+  end
+
+  defp do_build_tuple_list(prev, [], result), do: Enum.reverse(result)
+  defp do_build_tuple_list(prev, [target| rest], result) do
+     do_build_tuple_list(target, rest, [{prev, target} | result])
+  end
+
 end
--- test/dnsay_test.exs
+++ test/dnsay_test.exs
@@ -1,8 +1,31 @@
 defmodule DnsayTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  import Dnsay
   doctest Dnsay
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "Dnsay.transform_to_dns_valid_string/1" do
+    test "replaces spaces with configured character" do
+      assert transform_to_dns_valid_string("hello world") == "hello-world"
+    end
+    test "normalizes utf-8 characters" do
+      transformed = transform_to_dns_valid_string("árboles más grandes überfall")
+      assert transformed == "arboles-mas-grandes-uberfall"
+    end
+  end
+
+  describe "Dnsay.chunk/2" do
+    test "splits words into buckets of given size" do
+       assert chunk("hello world", 1) == ["hello", "world"]
+    end
+    test "splits big sentences into similar sized buckets" do
+       assert chunk("hello world foo bar baz", 3) == ["hello world foo", "bar baz"]
+    end
+  end
+
+  describe "Dnsay.build_tuple_list/2" do
+    test "build a tuple list with previous and target tuple" do
+      assert build_tuple_list(["a", "b"], "foo") == [{"foo", "a"}, {"a", "b"}]
+    end
   end
 end
